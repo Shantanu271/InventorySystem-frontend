d5622f3f8f288b2a39a1bc8ecfc32c8ec2168ac1
Chintu Sorte <sortechintu310@gmail.com>
2025-12-11 15:05:09 +0530

    enviornment files changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://inventorysystem-backend-de9k.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:8080/api'
+  production: false,
+  apiUrl: 'https://inventorysystem-backend-de9k.onrender.com/api'
 };
